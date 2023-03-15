e4209ea92c339907f067e36d2413ffda8e485c36
Quarrrtzzz <sid1.97@mail.ru>
2023-03-15 17:11:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+/* const category ='Dota2';
+
+console.log(`https://someurl.com/${category}/5`); */
+
+
+const user = "Dmitriy";
+
+alert(`Привет, ${user}`);
+
+console.log(4 + "5");
+
+
+
+/* incr++;
+decr--; */
+
+console.log(incr);
+console.log(decr);
+let incr = 10,
+    decr = 10;
